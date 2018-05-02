bd9c6722c40020d22416cd15f268d3189ab98dfd
YX\TC <919777596@qq.com>
2018-05-02 15:54:00 +0800

    add personal page

--- pages/personal.vue
+++ pages/personal.vue
@@ -1,58 +1,53 @@
 <template>
 	<div>
-		<Header2></Header2>
-		<el-row>
-			<el-col :xs="24" :sm="24" :md="{span:22,offset:1}" :lg="{span: 18,offset:3}" :xl="{span: 16,offset:4}">
-				<el-container style="padding: 30px 0;">
-				  <el-aside width="200px">
-					    <el-menu
-					      default-active="2"
-					      class="el-menu-vertical-demo"
-						>
-					      <el-submenu index="1">
-					        <template slot="title">
-					          <i class="el-icon-location"></i>
-					          <span>导航一</span>
-					        </template>
-					        <el-menu-item-group>
-					          <template slot="title">分组一</template>
-					          <el-menu-item index="1-1">选项1</el-menu-item>
-					          <el-menu-item index="1-2">选项2</el-menu-item>
-					        </el-menu-item-group>
-					        <el-menu-item-group title="分组2">
-					          <el-menu-item index="1-3">选项3</el-menu-item>
-					        </el-menu-item-group>
-					        <el-submenu index="1-4">
-					          <template slot="title">选项4</template>
-					          <el-menu-item index="1-4-1">选项1</el-menu-item>
-					        </el-submenu>
-					      </el-submenu>
-					      <el-menu-item index="2">
-					        <i class="el-icon-menu"></i>
-					        <span slot="title">导航二</span>
-					      </el-menu-item>
-					      <el-menu-item index="3" disabled>
-					        <i class="el-icon-document"></i>
-					        <span slot="title">导航三</span>
-					      </el-menu-item>
-					      <el-menu-item index="4">
-					        <i class="el-icon-setting"></i>
-					        <span slot="title">导航四</span>
-					      </el-menu-item>
-					    </el-menu>		  	
-				  </el-aside>
-				  <el-main>Main</el-main>
-				</el-container>				
-			</el-col>
-		</el-row>
-        <friend-link style="margin-top:140px"></friend-link>
-        <page-footer></page-footer>
-	</div>  
+	<Header2></Header2>
+	<div class="jl-box">
+		<div class="jl-loading" :class="showLoading ? '' : 'hide'">
+			<div class="loader">
+				Loading...
+			</div>
+		</div>
+		<div class="jl-wrap">
+			<div class="swiper-container">
+			    <div class="swiper-wrapper">
+			        <div class="swiper-slide jbxx">
+			        	<h5 class="jl-title ani" swiper-animate-effect="fadeIn" swiper-animate-duration="0.2s" swiper-animate-delay="0.1s">基本信息</h5>
+			        </div>
+			        <div class="swiper-slide grjj">
+			        	<div class="swiper-slide-inner">
+							<h5 class="jl-title">个人简介</h5>			        		
+			        	</div>
+			        </div>
+			        <div class="swiper-slide jsz">
+			        	<div class="swiper-slide-inner">
+			        		<h5 class="jl-title">技术栈</h5>
+			        	</div>
+			        </div>			        
+			        <div class="swiper-slide gzjl">
+			        	<div class="swiper-slide-inner">
+			        		<h5 class="jl-title">工作经历</h5>
+			        	</div>
+			        </div>
+			        <div class="swiper-slide xmjy">
+			        	<div class="swiper-slide-inner">
+			        		<h5 class="jl-title">项目经验</h5>
+			        	</div>
+			        </div>	
+			        <div class="swiper-slide xqah">
+			        	<div class="swiper-slide-inner">
+			        		<h5 class="jl-title">兴趣爱好</h5>
+			        	</div>
+			        </div>				        
+			    </div>
+			</div>
+		</div>
+        <!--<page-footer style="position: fixed;bottom: 0;"></page-footer>-->
+	</div>  		
+	</div>
 </template>
 <script>
 import Header2 from '../components/header2'
 import PageFooter from '../components/footer'
-import FriendLink from '../components/friendlink'
 export default {
     name: 'register',
 	data () {
@@ -60,7 +55,9 @@ export default {
 			activeName: '',
 			keywords: 'tuch',
 			description: 'tuch',
-            title: '屠春首页'   
+            title: '屠春首页',
+            swiper: null,
+            showLoading: true
 		}
 	},
 	head () {
@@ -69,14 +66,137 @@ export default {
 			meta: [
 				{ hid: 'description', name: 'description', content: this.description },
 				{ hid: 'keywords', name: 'keywords', content: this.keywords }
-			]
+			],
+			link: [
+				{href: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css'}
+			],
+	        script: [  
+	          { src: 'https://unpkg.com/scrollreveal/dist/scrollreveal.min.js' },
+	          { src: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js'},
+	          { src: '/swiper.animate1.0.2.min.js'}
+	        ] 			
 		}
 	},
 	components: {
-		Header2,
-        PageFooter,
-        FriendLink
-	}    
+		Header2
+	},
+	mounted () {			
+		setTimeout(()=>{
+			this.showLoading = false;
+		},1000)
+		let h = window.innerHeight;
+		document.querySelector('.jl-box').style.height = (h-41)+"px";
+		document.querySelector('.swiper-container').style.height = h+"px";
+		this.$nextTick(()=>{
+			this.swiper = new Swiper ('.swiper-container', {
+			    direction: 'vertical',
+			    loop: true,
+			    scrollbarHide: true,
+			    zoom: true,
+			    mousewheelControl : true,
+			    speed: 1000,
+			    grabCursor: true,
+			    onInit: function(swiper){ 
+				    swiperAnimateCache(swiper);
+				    swiperAnimate(swiper);
+			    }, 
+			    onSlideChangeEnd: function(swiper){ 
+			    	swiperAnimate(swiper);
+			    } 			    
+			})				
+		})
+	}
 }
 </script>
-
+<style lang="less">
+	.jl-box{
+		width: 100%;
+		box-sizing: border-box;
+		overflow: hidden;
+	}
+	.swiper-container {
+	    width: 100%;
+	}  	
+	.jl-title{
+		font-size: 20px;
+		text-align: center;
+		margin: 30px;
+	}
+	.swiper-slide{
+		&.jbxx{
+			background: linear-gradient(#ffffff, #0099CC 50%,#ffffff);
+		}
+		&.grjj{
+			background: linear-gradient(#ffffff, #339966 50%,#ffffff);
+		}	
+		&.jsz{
+			background: linear-gradient(#ffffff, #993366 50%,#ffffff);
+		}	
+		&.gzjl{
+			background: linear-gradient(#ffffff, #66CCCC 50%,#ffffff);
+		}		
+		&.xmjy{
+			background: linear-gradient(#ffffff, #663300 50%,#ffffff);
+		}		
+		&.xqah{
+			background: linear-gradient(#ffffff, #FF9933 50%,#ffffff);
+		}										
+	}
+	@keyframes loading{
+	  0%,100% {
+	    box-shadow: 0em -2.6em 0em 0em #ffffff, 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2), 2.5em 0em 0 0em rgba(255, 255, 255, 0.2), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.2), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2), -2.6em 0em 0 0em rgba(255, 255, 255, 0.5), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.7);
+	  }
+	  12.5% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.7), 1.8em -1.8em 0 0em #ffffff, 2.5em 0em 0 0em rgba(255, 255, 255, 0.2), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.2), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2), -2.6em 0em 0 0em rgba(255, 255, 255, 0.2), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.5);
+	  }
+	  25% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.5), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.7), 2.5em 0em 0 0em #ffffff, 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.2), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2), -2.6em 0em 0 0em rgba(255, 255, 255, 0.2), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+	  }
+	  37.5% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.2), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.5), 2.5em 0em 0 0em rgba(255, 255, 255, 0.7), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.2), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2), -2.6em 0em 0 0em rgba(255, 255, 255, 0.2), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+	  }
+	  50% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.2), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2), 2.5em 0em 0 0em rgba(255, 255, 255, 0.5), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.7), 0em 2.5em 0 0em #ffffff, -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2), -2.6em 0em 0 0em rgba(255, 255, 255, 0.2), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+	  }
+	  62.5% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.2), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2), 2.5em 0em 0 0em rgba(255, 255, 255, 0.2), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.5), 0em 2.5em 0 0em rgba(255, 255, 255, 0.7), -1.8em 1.8em 0 0em #ffffff, -2.6em 0em 0 0em rgba(255, 255, 255, 0.2), -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+	  }
+	  75% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.2), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2), 2.5em 0em 0 0em rgba(255, 255, 255, 0.2), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.5), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.7), -2.6em 0em 0 0em #ffffff, -1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+	  }
+	  87.5% {
+	    box-shadow: 0em -2.6em 0em 0em rgba(255, 255, 255, 0.2), 1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2), 2.5em 0em 0 0em rgba(255, 255, 255, 0.2), 1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2), 0em 2.5em 0 0em rgba(255, 255, 255, 0.2), -1.8em 1.8em 0 0em rgba(255, 255, 255, 0.5), -2.6em 0em 0 0em rgba(255, 255, 255, 0.7), -1.8em -1.8em 0 0em #ffffff;
+	  }
+	}
+	.jl-loading{
+		position: fixed;
+		z-index: 99999999;
+		background: #009966;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		transition: all .5s linear; 
+		&.hide{
+			transform: scaleY(0);
+			transform-origin: center 0;
+		}
+		.loader{
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50,-50%);
+		    font-size: 25px;
+		    width: 1em;
+		    height: 1em;
+		    border-radius: 50%;
+		    position: relative;
+		    text-indent: -9999em;
+		    -webkit-animation: loading 1.1s infinite ease;
+		    animation: loading 1.1s infinite ease;			
+		}
+	}
+	.swiper-slide-inner{
+		
+	}
+</style>
